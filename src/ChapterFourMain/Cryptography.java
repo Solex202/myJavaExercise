5ebde6362c3a1434d8048d9e4a7b5e8af4b19545
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-01-01 17:19:52 +0100

    new year exercise

--- src/ChapterFourMain/Cryptography.java
+++ src/ChapterFourMain/Cryptography.java
@@ -7,29 +7,48 @@ public class Cryptography {
 
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter a valid number: ");
+        System.out.println("Enter a valid number: ");
         int digit = scan.nextInt();
+        String stringDigit = "" + digit;
+//        System.out.println(stringDigit.length());
 
-        int firstDigits = digit / 10000;
-        System.out.println(firstDigits);
+       while (stringDigit.length() != 4){
+           System.out.println("invalid digit");
+           System.out.println("Enter a valid number:");
+           digit = scan.nextInt();
+           stringDigit = "" + digit;
+       }
+           int firstDigits = digit / 1000;
+           //System.out.println(firstDigits);
+           int firstDigits2 = (firstDigits + 7) % 10;
 
-        int secondDigit = (digit % 10000) / 1000;
-        System.out.println(secondDigit);
+           int secondDigit = (digit % 1000) / 100;
+           // System.out.println(secondDigit);
+           int secondDigit2 = (secondDigit + 7) % 10;
 
-        int thirdDigit = (digit % 1000) / 100;
-        System.out.println(thirdDigit);
+           int thirdDigit = (digit % 100) / 10;
+           //System.out.println(thirdDigit);
+           int thirdDigit2 = (thirdDigit + 7) % 10;
 
-        int fourthDigit = (digit % 100) / 10;
-        System.out.println(fourthDigit);
-
-        int fifthDigit = (digit % 10) ;
-        System.out.println(fifthDigit);
-
-        System.out.printf(" %d  %d  %d  %d  %d",
-                firstDigits, secondDigit, thirdDigit, fourthDigit, fifthDigit);
+           int fourthDigit = digit % 10;
+           //System.out.println(fourthDigit);
+           int fourthDigit2 = (fourthDigit + 7) % 10;
 
 
-    }
+            int tempFirst = firstDigits2;
+            firstDigits2 = thirdDigit2;
+            thirdDigit2 = tempFirst;
+
+            int tempSecond = secondDigit2;
+            secondDigit2 = fourthDigit2;
+            fourthDigit2 = tempSecond;
+
+
+            System.out.printf(" %d  %d  %d  %d  ",
+                    firstDigits2, secondDigit2, thirdDigit2, fourthDigit2);
+
+        }
+
 
 
 
