bbd5e056d75b6167547619533fdbfc6e12b65149
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-01-05 21:51:10 +0100

    compound interest

--- src/ChapterFive/CompoundInterest.java
+++ src/ChapterFive/CompoundInterest.java
@@ -1,2 +1,25 @@
-package ChapterFive;public class CompoundInterest {
+package ChapterFive;
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+
+        double amount;
+        double principal = 1000.0;
+       // double rate = 0.05 - 0.10;
+
+
+        System.out.printf("%s%25s%25s %n", "Year","interest rate", "Amount on deposit");
+
+
+        for (int year = 1; year <= 10; year++) {
+            for (int rate = 5 ; rate <= 10; rate++) {
+
+
+                amount = principal * Math.pow(1.0 + (rate*0.01), year);
+
+
+                System.out.printf("%2d%20d%25.2f%n", year,rate, amount);
+            }
+        }
+    }
 }
