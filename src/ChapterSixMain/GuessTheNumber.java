e60d0a43e3a803b63d7241343b1a05931169ff59
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-01-14 21:25:56 +0100

    my dietel exercise

---
+++ src/ChapterSixMain/GuessTheNumber.java
@@ -0,0 +1,72 @@
+package ChapterSixMain;
+
+import java.util.Scanner;
+
+public class GuessTheNumber {
+        private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+//        int min = 1;
+//        int max = 1000;
+//        int guessNumber;
+//        guessNumber = (int)(Math.random() * (max - min + 1) + 1);
+//        System.out.println(  guessNumber);
+
+        System.out.println("Guess the number: ");
+        int number = scan.nextInt();
+
+        while (number != -1){
+            int random = randomGenerator();
+
+            numberGuessing(number, random);
+        }
+
+
+    }
+    public static void  numberGuessing(int number, int guessNumber) {
+
+//        while (number != -1) {
+//          if (number >= 1 && number <= 1000) {
+//              System.out.println("number out of bound");
+//              System.out.println("Guess a number");
+//
+//          }
+            if (number > guessNumber) {
+              System.out.println("Too high.Try again");
+              System.out.println("Guess the number: ");
+              number = scan.nextInt();
+          } else
+              if (number < guessNumber) {
+              System.out.println("Too low.Try again");
+              System.out.println("Guess the number: ");
+              number = scan.nextInt();
+          } else {
+              System.out.println("Congratulations.You guessed right!!!");
+
+
+          }
+            System.out.println("Do you wish to continue\n yes \n no?");
+            String userInput = scan.nextLine();
+            if (userInput == "yes") randomGenerator();
+            else System.exit(0);
+
+
+    }
+    public static int randomGenerator(){
+        int min = 1;
+        int max = 1000;
+        int guessNumber;
+        guessNumber = (int)(Math.random() * (max - min + 1) + 1);
+        System.out.println(  guessNumber);
+
+
+        return guessNumber;
+    }
+
+    }
+
+
+
+
+
+
