9fcfbbcd17e4ebe913ad36d615e51ef6e0cd11f3
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-02-16 09:00:59 +0100

    my dietel exercise

---
+++ src/ChapterSixMain/CoinTossing.java
@@ -0,0 +1,43 @@
+package ChapterSixMain;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class CoinTossing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                MENU:
+                Press 1 to to toss a coin
+                Press 2 to exit program""");
+
+        int choice = scanner.nextInt();
+        int count = 0;
+        while (choice == 1){
+            Coin coinFace = flip();
+            System.out.println("You got a "+ coinFace);
+            count++;
+            System.out.println("""
+                MENU:
+                Press 1 to to toss a coin
+                Press 2 to exit program""");
+
+            choice = scanner.nextInt();
+        }
+
+        System.out.println("The coin was tossed "+ count + " times");
+
+    }
+
+    private static Coin flip() {
+        SecureRandom random = new SecureRandom();
+        int randomValue = random.nextInt(2);
+        if (randomValue == 0){
+            return Coin.HEADS;
+        }
+        else{
+            return Coin.TAILS;
+        }
+    }
+
+}
