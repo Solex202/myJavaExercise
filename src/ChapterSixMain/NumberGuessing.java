c57014886dba7a05e2754a849bcac8427b9153d8
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-02-23 09:31:35 +0100

    my java exercise

--- src/ChapterSixMain/NumberGuessing.java
+++ src/ChapterSixMain/NumberGuessing.java
@@ -1,55 +1,89 @@
 package ChapterSixMain;
 
 import java.security.SecureRandom;
+import java.util.InputMismatchException;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuessing {
     private static final Scanner scanner = new Scanner(System.in);
+    private static int tryCount = 0;
+
     public static void main(String[] args) {
-
-
-
-        int tryCount = 0;
-        do {
-
-            randomCheck(getRandomNumbers(), tryCount);
-            System.out.println("Do you want to continue: ");
-            String userInput = scanner.next();
-            if(Objects.equals(userInput, "yes")){
-                randomCheck(getRandomNumbers(),tryCount);
+        try {
+            do {
+                randomCheck(getRandomNumbers(), tryCount);
+                System.out.println("Do you wish to continue: ");
+                String userInput = scanner.next();
+                if(Objects.equals(userInput, "yes")){
+                    randomCheck(getRandomNumbers(),tryCount);
+                }
+                else if(Objects.equals(userInput, "no")){
+                    System.out.println("goodbye!!!");
+                    System.exit(0);
+                }
             }
-            else {
-                System.exit(0);
-            }
+            while(true);
+        }catch (InputMismatchException e){
+            System.out.println(e.getMessage());
         }
-        while(true);
+
     }
 
     private static void randomCheck(int randomNumbers, int tryCount) {
-        while(true){
-            display("guess a number");
+        while (true) {
+            try {
+                action(randomNumbers, tryCount);
+
+            }
+            catch (InputMismatchException e){
+                System.out.println("Caught Exception");
+                action(randomNumbers, tryCount);
+            }
+            tryCount++;
+        }
+    }
+
+    private static void action(int randomNumbers, int tryCount) {
+        Scanner scanner = new Scanner(System.in);
+        display("guess a number: ");
+        //System.out.println("TryCount --> " + tryCount);
         int userInput = scanner.nextInt();
-        tryCount++;
-
-
-        if(userInput > randomNumbers){
+        if (userInput >= 0 && userInput <= 1000) {
+            // }
+            // catch(InputMismatchException e){
+            if (userInput > randomNumbers) {
                 display("too high,try again!");
-            }
-        else if(userInput < randomNumbers){
+            } else if (userInput < randomNumbers) {
                 display("too low,try again!");
-            }
-        else {
-                display("yo,you won");
+            } else {
+                display("yo,Congratulation you won");
                 display("you had " + tryCount + " tries");
-                break;
+                countGuess(tryCount);
             }
+        } else {
+            throw new InputMismatchException("something went wrong\ninvalid digit");
+
+        }
+        System.out.println("TryCount --> " + tryCount);
+
+    }
+
+    private static void countGuess(int tryCount) {
+        if(tryCount <= 10){
+            display("either you know the secret or you got lucky");
+        }
+        else if(tryCount > 10){
+            display("you should be able to do better,why should it take you more than 10 guesses?");
+        }
+        else {
+            display("Aha! you know the secret");
         }
     }
 
     private static int getRandomNumbers() {
-        Random rand = new Random();
+        SecureRandom rand = new SecureRandom();
         return rand.nextInt(1000) + 1;
     }
 
