179e41cf4dc62478858d7dacdd38545002cfc702
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-02-23 20:53:30 +0100

    my java exercise

---
+++ src/ChapterSeven/ArrayPower.java
@@ -0,0 +1,26 @@
+package ChapterSeven;
+
+import java.util.Arrays;
+
+public class ArrayPower   {
+    public static void main(String[] args) {
+        double  [] value = {5.3, 4.3, 1.5, 7.3, 2.9,6.8, 4.9, 7.0};
+        double  [] arr = powArray(value, 2);
+        printArray(arr);
+    }
+
+    public static double [] powArray(double [] a, int power){
+        for (int i = 0; i < a.length; i++) {
+          a[i] = Math.pow(a[i],power);
+        }
+        return a;
+    }
+
+    public static void printArray(double [] a) {
+        System.out.println("{");
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%.2f"+" ", a[i] );
+        }
+        System.out.println("}");
+    }
+}
