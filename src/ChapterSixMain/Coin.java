ef07ddcf88841db52eb5c8c2b46b525fc72a3d90
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-02-23 09:29:02 +0100

    my java exercise

--- src/ChapterSixMain/Coin.java
+++ src/ChapterSixMain/Coin.java
@@ -4,4 +4,5 @@ import java.util.Scanner;
 
 public enum Coin {
     HEADS,TAILS;
+    //  https://www.coursera.org/lecture/cs-programming-java/turtle-graphics-Dj3Bb?utm_source=link&utm_medium=page_share&utm_content=vlp&utm_campaign=top_button
 }
