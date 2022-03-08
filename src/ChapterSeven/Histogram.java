3c354b0a1a0b3feca0cf97d45604f5fcbf3867a3
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-03-08 18:41:24 +0100

    my java exercise

---
+++ src/ChapterSeven/Histogram.java
@@ -0,0 +1,22 @@
+package ChapterSeven;
+
+public class Histogram {
+
+    public static void main(String[] args) {
+        System.out.print("Roman alphabet: ");
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c);
+        }
+        System.out.println();
+
+
+        System.out.print("Greek alphabet: ");
+        for (int i = 913; i <= 937; i++) {
+            System.out.print((char) i);
+        }
+        System.out.println();
+    }
+//    public static int histoMethod(){
+//
+//    }
+}
