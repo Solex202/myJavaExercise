a8df78440a7d3ea25c166738b9e3f448357035dc
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-03-26 18:58:08 +0100

    deitel exercise

---
+++ src/chapterFourteen/ConvertingValues.java
@@ -0,0 +1,14 @@
+package chapterFourteen;
+
+import java.util.Scanner;
+
+public class ConvertingValues {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a value");
+        int _char = scanner.nextInt();
+        char input = (char) _char;
+        System.out.println(input);
+    }
+}
