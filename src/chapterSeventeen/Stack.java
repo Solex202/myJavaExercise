4da58f5129e3bc443730135bf15e5de0b77a12d4
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-03 15:58:36 +0100

    my dietel ex3rcise

---
+++ src/chapterSeventeen/Stack.java
@@ -0,0 +1,64 @@
+package chapterSeventeen;
+
+public class Stack {
+
+    static final int MAX = 1000;
+    int top;
+    int[] a = new int[MAX];
+
+    boolean isEmpty(){
+        return (top < 0);
+    }
+
+    Stack(){
+        top = -1;
+
+    }
+
+    boolean push (int x){
+        if(top >= (MAX -1 )){
+            System.out.println("stack overflow");
+            return false;
+        }
+        else {
+            a[++top] = x;
+            System.out.println(x + "pushed into stack");
+            return true;
+        }
+    }
+
+    int pop(){
+        if(top < 0){
+            System.out.println("stack underflow");
+            return 0;
+        }
+        else {
+            int x = a[top--];
+            return x;
+        }
+    }
+
+    int peek(){
+        if(top < 0){
+            System.out.println("stack underflow");
+            return 0;
+        }
+        else {
+            int x = a[top];
+            return x;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+//        s.pop();
+
+//        System.out.println(s.pop() + "popped from stack");
+//        System.out.println(s.peek());
+        System.out.println(s.isEmpty());
+    }
+
+}
