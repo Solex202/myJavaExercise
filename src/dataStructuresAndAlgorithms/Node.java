479d124b12512021ea5d9384525cda451a222758
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-03 15:55:22 +0100

    my dietel ex3rcise

---
+++ src/dataStructuresAndAlgorithms/Node.java
@@ -0,0 +1,10 @@
+package dataStructuresAndAlgorithms;
+
+public class Node {
+    private int value;
+    private Node next;
+
+    public void setValue(int value){
+        this.value = value;
+    }
+}
