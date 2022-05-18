a6aebf40f615f9369abdfc3d8f62c39d5013cb31
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-18 21:45:46 +0100

    new update

---
+++ src/ChapterSevenMain/DuplicationElimination.java
@@ -0,0 +1,12 @@
+package ChapterSevenMain;
+
+public class DuplicationElimination {
+    public int removeDuplicate(int[] storedArray) {
+        for (int i = 0; i < storedArray.length; i++) {
+//            if
+
+
+        }
+        return 9;
+    }
+}
