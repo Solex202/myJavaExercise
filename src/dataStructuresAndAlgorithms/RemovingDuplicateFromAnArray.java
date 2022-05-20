28e76d7943fcce5501020af26cb4c035efb3e28d
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-20 21:18:36 +0100

    new commit

---
+++ src/dataStructuresAndAlgorithms/RemovingDuplicateFromAnArray.java
@@ -0,0 +1,17 @@
+package dataStructuresAndAlgorithms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemovingDuplicateFromAnArray {
+
+    public static void main(String[] args) {
+        int [] array = {1,1,2,3,3,4,5,5,6,7};
+        int [] newArray = {};
+        Set<Integer> arrays = new HashSet<>();
+
+        for (int i = 0; i < array.length; i++) {
+
+        }
+    }
+}
