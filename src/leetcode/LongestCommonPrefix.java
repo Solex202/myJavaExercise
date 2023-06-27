7a9cd91a4db3a53df7064d7e83634b3142f0117a
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-27 06:18:21 +0100

    solving problems

---
+++ src/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        String [] arr = {"flourish", "flow", "floight"};
+        String res = commonPrefix(arr);
+        System.out.println(res);
+    }
+    public static String commonPrefix(String [] arr){
+        var builder = new StringBuilder();
+
+        if(arr == null || arr.length == 0) return builder.toString();
+
+        int lengthOfSmallestElement = arr[0].length();
+
+        for (String s : arr) {
+            lengthOfSmallestElement = Math.min(lengthOfSmallestElement, s.length());
+        }
+
+        for (int i = 0; i < lengthOfSmallestElement; i++){
+            char current = arr[0].charAt(i);
+
+            for (String word: arr) {
+                if(current != word.charAt(i)){
+                    return builder.toString();
+                }
+            }
+            builder.append(current);
+        }
+        
+        return builder.toString();
+    }
+}
