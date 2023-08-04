83daa55734494f282d6f196fbba10a63fb2f8afc
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-08-04 16:27:11 +0100

    leetcode problems

---
+++ src/leetcode/arrays/LengthOfLastWordInAString.java
@@ -0,0 +1,24 @@
+package leetcode.arrays;
+
+public class LengthOfLastWordInAString {
+
+    public static void main(String[] args) {
+        String word = "my name is deola    ";
+        int length = lengthOfLastWord(word);
+        System.out.println(length);
+    }
+
+    public static int lengthOfLastWord(String s){
+        int right = s.length() -1 ;
+        while (right >= 0 && s.charAt(right) == ' '){
+            right --;
+        }
+
+        int left = right;
+        while (left >= 0 && s.charAt(left) != ' '){
+            left--;
+        }
+
+        return right - left;
+    }
+}
