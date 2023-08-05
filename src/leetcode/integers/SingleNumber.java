5f59ca19beca2ae356a68ad8a2c168259f72a5a7
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-08-05 22:56:00 +0100

    leetcode problems

--- src/leetcode/integers/SingleNumber.java
+++ src/leetcode/integers/SingleNumber.java
@@ -1,4 +1,28 @@
 package leetcode.integers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SingleNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int singleNumber(int [] arr){
+        if(arr.length == 0){
+            return arr[0];
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++){
+            if(set.contains(arr[i])){
+                set.remove(arr[i]);
+            }else if(!set.contains(arr[i])){
+                set.add(arr[i]);
+            }
+        }
+
+        return set.iterator().next();
+    }
 }
