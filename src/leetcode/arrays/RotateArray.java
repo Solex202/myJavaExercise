2a49969896818a5f14a5caf3c6685f1d7bc9d6e2
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-08-06 23:13:42 +0100

    leetcode problems

--- src/leetcode/arrays/RotateArray.java
+++ src/leetcode/arrays/RotateArray.java
@@ -1,10 +1,25 @@
 package leetcode.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RotateArray {
+    public static void main(String[] args) {
+        int [] arr = {2,1,3,5};
+        rotateArray(arr, 2);
+//        System.out.println(res);
+    }
+    public static void rotateArray(int [] array, int step){
 
-    public int [] rotateArray(int [] array, int step){
+        List<Integer> rot = new ArrayList<>();
 
+        for (int i = array.length-1; i >= 0; i--) {
+            rot.add(array[i]);
+        }
 
-        return array;
+//        int [] arr =
+//
+//        System.out.println(rot);
+
     }
 }
