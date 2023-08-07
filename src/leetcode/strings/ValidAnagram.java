b493066f7638586a363d371529dad8c36b30c031
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-08-07 23:10:36 +0100

    leetcode problems

--- src/leetcode/strings/ValidAnagram.java
+++ src/leetcode/strings/ValidAnagram.java
@@ -1,4 +1,33 @@
 package leetcode.strings;
 
 public class ValidAnagram {
+
+    public static void main(String[] args) {
+
+    String first = "tea";
+    String second = "eat ";
+
+    boolean isAnagram = isAnagram(first,second);
+
+        System.out.println(isAnagram);
+    }
+
+    public static boolean isAnagram(String first, String second){
+        if(first.length() != second.length()){
+            return false;
+        }
+
+        int [] counter = new int[26];
+        for (int i = 0; i < first.length()-1; i++) {
+            counter[first.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < second.length()-1; i++) {
+            counter[second.charAt(i) - 'a']--;
+            if (counter[second.charAt(i) - 'a'] < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
