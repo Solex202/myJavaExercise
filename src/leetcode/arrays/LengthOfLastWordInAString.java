9345db3165260e8911200a82b9559b0eb5d33123
lota <christiansolomon592@gmail.com>
2025-03-12 10:54:12 +0100

    adding lombok to class path

--- src/leetcode/arrays/LengthOfLastWordInAString.java
+++ src/leetcode/arrays/LengthOfLastWordInAString.java
@@ -3,7 +3,7 @@ package leetcode.arrays;
 public class LengthOfLastWordInAString {
 
     public static void main(String[] args) {
-        String word = "my name is deola    ";
+        String word = "my name is deola";
         int length = lengthOfLastWord(word);
         System.out.println(length);
     }
