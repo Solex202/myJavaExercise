256650112250b1636a7cc3197cbd85823a7798a3
lota <christiansolomon592@gmail.com>
2025-03-13 11:29:28 +0100

    Maximal Score After Applying K Operations

--- src/leetcode/arrays/MaximalScore.java
+++ src/leetcode/arrays/MaximalScore.java
@@ -6,7 +6,9 @@ public class MaximalScore {
     public int maxScore(int[] nums, int loopCount) {
 
         PriorityQueue <Integer> heapPush = new PriorityQueue<>((a, b) -> b - a);
-
+        if (nums.length == 0 || loopCount <= 0) {
+            return 0;
+        }
         for (int num : nums) {
             heapPush.offer(num);
             System.out.println(heapPush);
@@ -14,7 +16,7 @@ public class MaximalScore {
 
         int sum = 0;
         for (int j = 0; j < loopCount ; j++) {
-            int maxCount = heapPush.poll();
+            int maxCount = !heapPush.isEmpty() ? heapPush.poll() : 0;
             sum  += maxCount;
             System.out.println("The sum is: " + sum);
             heapPush.offer((int) Math.ceil(maxCount/3.0));
