2d5ed39d7c7a03d3637807fe8fa7b9600ab45614
lota <christiansolomon592@gmail.com>
2025-03-18 11:49:06 +0100

    solving returning length of last word

--- test/leetcode/LengthOfLastWordTest.java
+++ test/leetcode/LengthOfLastWordTest.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import leetcode.arrays.LengthOfLastWordInAString;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -15,23 +16,23 @@ class LengthOfLastWordTest {
             "Hello World , 5",
             " Hello World, 5",
             " Hello World , 5",})
-    void testLengthOfLastIsReturnedSuccessfully1(String input, int expected) {
+    void testLengthOfLastIsReturnedSuccessfully(String input, int expected) {
         LengthOfLastWordInAString lengthOfLastWord = new LengthOfLastWordInAString();
         int result = lengthOfLastWord.lengthOfLastWord(input);
-        assertEquals(expected, result);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     void testLengthIsZeroWhenStringIsEmpty() {
         LengthOfLastWordInAString lengthOfLastWord = new LengthOfLastWordInAString();
         int result = lengthOfLastWord.lengthOfLastWord2("");
-        assertEquals(0, result);
+        Assertions.assertEquals(0, result);
     }
 
     @Test
     void testLengthIsZeroWhenStringIsNull() {
         LengthOfLastWordInAString lengthOfLastWord = new LengthOfLastWordInAString();
         int result = lengthOfLastWord.lengthOfLastWord2(null);
-        assertEquals(0, result);
+        Assertions.assertEquals(0, result);
     }
 }
