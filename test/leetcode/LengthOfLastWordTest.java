e794227098d650d5ad9af0194d0a7d03e8202b6c
lota <christiansolomon592@gmail.com>
2025-03-18 10:43:34 +0100

    solving returning length of last word

---
+++ test/leetcode/LengthOfLastWordTest.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import leetcode.arrays.LengthOfLastWordInAString;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class LengthOfLastWordTest {
+
+    @Test
+     void testLengthOfLastIsReturnedSuccessfully() {
+        LengthOfLastWordInAString lengthOfLastWord = new LengthOfLastWordInAString();
+        int result = lengthOfLastWord.lengthOfLastWord("Hello World");
+        assertEquals(5, result);
+    }
+
+
+}
